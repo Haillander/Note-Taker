6ced8578a10738228161579395313e45ab529ef5
Haillander <haillanderviana@gmail.com>
2023-02-06 17:37:48 +1100

    server routes and express settings

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+//EXPRESS SETTINGS 
 const express = require('express')
-const apiRoutes = require('./routes/apiRoutes')
-const  htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/RoutesAPI')
+const  htmlRoutes = require('./routes/rotesHTML')
 const app = express()
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-app.use('/api', apiRoutes)
-app.use('/', htmlRoutes)
+//ROUTES 
 
-app.listen(PORT, () => console.log(`We are live listening on PORT: ${PORT}`))
+require('./routes/RoutesAPI')(app);
+require('./routes/routesHTML')(app);
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+});
